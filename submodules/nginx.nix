111e63798b7f7b479c042302e233d276dc38c92a
FokoHetman <paprykkania@gmail.com>
2025-07-12 19:43:57 +0200

    yo stupido

--- submodules/nginx.nix
+++ submodules/nginx.nix
@@ -11,7 +11,7 @@
           proxyPass = "http://localhost:2137";
           proxyWebsockets = true;
           extraConfig = ''
-            proxy_set_header = X-Real-IP $remote_addr;
+            proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           '';
         };
