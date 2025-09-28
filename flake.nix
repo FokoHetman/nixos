8ca793668bf3f18c5c9c21808d1e694200085fb0
FokoHetman <paprykkania@gmail.com>
2025-09-28 18:54:28 +0200

    packaging xmo*

--- flake.nix
+++ flake.nix
@@ -98,6 +98,10 @@
       name = "foko";
       inherit uid canSudo;
     };
+
+    xmonad.package = pkgs.callPackage ../assets/xmonad {shell=false;};
+    xmobar.package = pkgs.callPackage ../assets/xmobar {shell=false;};
+
     nvim.packages = rec {
       nvimBuilder = pkgs: (inputs.nvf.lib.neovimConfiguration {
         inherit pkgs;
@@ -111,9 +115,6 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCjpL3XBTmxH6biR7rwIviQcA5q3WygfLriAv6+fLA566Bg7kmfHso3lXEm9zIf//EM+np15venbyg35kwf7jDbyPr80QrdQJFQacKL4KAWxj739uPdS5XCdwF2Lf4yHcOncdPz5vupgcQM7qlF/U3Xt2HoqXfb+7nnFZPgwiJ6xP81FltEQmhQRrj0vwK4aVu4VyZ2/7PqmAnbmo42OVYpkTcIjmFpOfnXpw/m3VqoLiNyDPra4LkhzL+umWEUqBtwqkZMG5rP/HNMll7u3AoCfbVwFJg4cjjUYy/uDE8PAZP9xrWQva6kCqH6YR4iUJpKXUMtQRqg1z+/e/QkjtehFnrwd4HOLK1+LBGgbQ4j7duyfblR5yKEYP3C8mGEKB5yqo3si26nzPxUzZvjT7XAoG02KypGHUeeZ9hJ7NbXdxaycmvRsRj4OSoXmexo1r6qY8RckPzAsqpdrDWFcoeePVcRR6Yc3P5dc7NDzXh0FQ55VViQTLqlDgdtETdPd17Vqa1RIFNr/sn0VMJJnXqno3ViEUl8b2LvRQBWKKJWy+oymTwnT3bxN9BFHpbEKK5zzOd4H8/qo7UtUBrCtr2WzSxnnmLQWwRguw9ysfkWdjMChNCwU4GjSBYy/VBkoO4sH9Phf1RNMRvoA6el4lRPn56qhIOeVKX+V0IzMeOoBw== foko@hetman.at"
     ];
     
-    assets = ./assets;
-    submodules = ./submodules;
-
     fonts.rainworld = pkgs.callPackage({ pkgs }: pkgs.stdenv.mkDerivation {
       name = "rainworld-font";
       dontConfigure = true;
