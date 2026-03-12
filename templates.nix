da4afc8443429f6fd8fb7f2c381cf79b931f90b0
FokoHetman <paprykkania@gmail.com>
2026-03-12 18:54:27 +0100

    better template writter

--- templates.nix
+++ templates.nix
@@ -1,41 +1,20 @@
 {lib, inputs, architectures, nixpkgsConfig, ...}:
-  let mkPkgs = arch : import inputs.nixpkgs ({ system = arch; }//nixpkgsConfig);
+let 
+  mkPkgs = arch : import inputs.nixpkgs ({ system = arch; }//nixpkgsConfig);
+  inheritance = name: def: {
+    preconf = x: x;
+    builder = x: {${name} = x;};
+    default = def;
+  };
 in rec {
   perSystem = {
     preconf = x: builtins.mapAttrs (name: f: builtins.listToAttrs (map (arch: {name = arch; value = f {pkgs=mkPkgs arch;};}) architectures)) x;
     builder = x: x;
     default = {};
   };
-  nixosModules = {
-    preconf = x: x;
-    builder = modules : {nixosModules = modules;};
-    default = {};
-  };
-  nixosConfigurations = {
-    preconf = x: x;
-    builder = configurations : {nixosConfigurations = configurations;};
-    default = {};
-  };
-  homeModules = {
-    preconf = x: x;
-    builder = modules: {homeModules = modules;};
-    default = {};
-  };
-  homeConfigurations = {
-    preconf = x: x;
-    builder = configurations: {homeConfigurations = configurations;};
-    default = {};
-  };
-  globals = {
-    preconf = x: x;
-    builder = x: {globals = x;};
-    default = {};
-  };
-  /*flake.perSystem.packages = {pkgs, ...}: {
-    hello = pkgs.hello;
-  };
-  /*flake.templates.hjemConfiguration = {
-    builder = configurations : {nixosConfigurations = configurations;};
-    default = {};
-  };*/
+  nixosModules = inheritance "nixosModules" {};
+  nixosConfigurations = inheritance "nixosConfigurations" {};
+  homeModules = inheritance "homeModules" {};
+  homeConfigurations = inheritance "homeConfigurations" {};
+  globals = inheritance "globals" {};
 }
